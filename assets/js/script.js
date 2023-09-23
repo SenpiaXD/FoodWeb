a4ffd09d753013e4812ee0f153b98d6f46250a65
Koni <145802334+SenpiaXD@users.noreply.github.com>
2023-09-23 23:22:44 +0800

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 const openMenuButton = document.getElementById("open-menu");
 const closeMenuButton = document.getElementById("close-menu");
 const ul = document.getElementById("nav-ul");
+const home = document.getElementById("home");
 
 openMenuButton.addEventListener("click", () => {
     closeMenuButton.style.visibility = "visible";
     ul.style.display = "block";
     closeMenuButton.style.display = "block";
     openMenuButton.style.display = "none";
+    home.style.paddingTop = "200px";
 });
 
 closeMenuButton.addEventListener("click", () => {
     ul.style.display = "none";
     closeMenuButton.style.display = "none";
     openMenuButton.style.display = "block";
+    home.style.paddingTop = "85px";
 });
