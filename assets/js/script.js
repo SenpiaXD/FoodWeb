c924465b9b9fd1f3808f7e71e5f5698b1bb5a840
Koni <145802334+SenpiaXD@users.noreply.github.com>
2023-10-04 01:47:38 +0800

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,41 +11,33 @@ document.addEventListener("DOMContentLoaded", function () {
         toggleMenuButton.classList.toggle("active");
 
         if (navUl.classList.contains("show")) {
-            home.style.paddingTop = "310px";
+            // home.style.paddingTop = "310px";
         } else {
             home.style.paddingTop = "85px";
         }
 
         window.addEventListener("resize", function () {
             if (window.innerWidth <= 769) {
-                home.style.paddingTop = "250px";
+                // home.style.paddingTop = "250px";
             } else {
-                home.style.paddingTop = "200px";
+                //home.style.paddingTop = "200px";
             }
         });
     });
 });
 
+// Mobile Functionality: Responsive Navbar
+const listView = document.getElementById("nav-ul");
+
+listView.addEventListener("click", () => {
+    listView.classList.toggle("show");
+    home.style.paddingTop = "85px";
+});
+
 // Functionality for Every View: Modal View 
 
-var jsonDataArray = [
-    {
-        name: "Ronnel Cepada",
-        strand: "Culinary Arts",
-        id: "02000123456",
-        role: "Researcher",
-        imageUrl: "./assets/img/ronnel.jpg"
-    },
-    {
-        name: "Fernando Villanueva Jr.",
-        strand: "Information Technology",
-        id: "02000567329",
-        role: "FrontEnd/BackEnd",
-        imageUrl: "./assets/img/shizuku.jpg"
-    },
-];
-
 function modalShow(cardIndex) {
+
     var modal = document.getElementById("modal");
 
     var modalTitle = modal.querySelector(".modal-title");
@@ -56,17 +48,17 @@ function modalShow(cardIndex) {
 
     var selectedData = jsonDataArray[cardIndex];
 
-    modalTitle.textContent = selectedData.name;
-    modalStrand.textContent = selectedData.strand;
-    modalId.textContent = "ID: " + selectedData.id;
-    modalRole.textContent = "Worked: " + selectedData.role;
+    modalTitle.innerHTML = selectedData.name;
+    modalStrand.innerHTML = selectedData.strand;
+    modalId.innerHTML = "<b><i>ID: </i></b>" + selectedData.id;
+    modalRole.innerHTML = "<b>Worked on: </b>" + selectedData.role;
 
     modalImage.src = selectedData.imageUrl;
 
     modal.style.display = "block";
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("click", function () {
     var closeButton = document.querySelector(".modal .btn-close");
     if (closeButton) {
         closeButton.addEventListener("click", closeModal);
@@ -77,3 +69,96 @@ function closeModal() {
     var modal = document.getElementById("modal");
     modal.style.display = "none";
 }
+
+//Internal JSON
+var jsonDataArray = [
+    // Leader of the Research/System
+    {
+        name: "Ronnel Cepada",
+        strand: "Culinary Arts",
+        id: "02000123456",
+        role: "Leader of Culinary/Researcher/Designer",
+        imageUrl: "../assets/img/Members/ronnel.jpg"
+    },
+    {
+        name: "Fernando Villanueva Jr.",
+        strand: "Information Technology",
+        id: "02000567329",
+        role: "Leader of IT/FrontEnd/BackEnd/Researcher",
+        imageUrl: "./assets/img/Members/fernando.jpg"
+    },
+    // ----------------------End-------------------------
+    {
+        //Member of Culinary Index 2
+        name: "Mark Vincent",
+        strand: "Culinary Arts",
+        id: "",
+        role: "Researcher",
+        imageUrl: "",
+    },
+    {
+         //Member of Culinary Index 3
+        name: "Jozh Ryle Jimenez",
+        strand: "Culinary Arts",
+        id: "",
+        role: "Researcher",
+        imageUrl: "",
+    },  
+    {   
+        //Member of Culinary  Index 4
+        name: "James Gesto",
+        strand: "Culinary Arts",
+        id: "",
+        role: "Researcher",
+        imageUrl: "",
+    },
+    {   
+         //Member of IT  Index 5
+        name: "memeber of culinary",
+        strand: "Culinary Arts",
+        id: "",
+        role: "Researcher",
+        imageUrl: ""
+    },
+    {
+         //Member of IT  Index 6
+        name: "Argie Delgado",
+        strand: "Culinary Arts",
+        id: "02000287369",
+        role: "FrontEnd/BackEnd/Researcher",
+        imageUrl: "",
+    },
+    {
+         //Member of IT  Index 7
+        name: "Ryan Consigna",
+        strand: "Information Technology",
+        id: "02000303132",
+        role: "Researcher/Designer",
+        imageUrl: "",
+    },
+    {
+        //Member of IT  Index 8
+        name: "Christopher Fegalan",
+        strand: "Culinary Arts",
+        id: "02000306245",
+        role: "FrontEnd/BackEnd/Researcher",
+        imageUrl: "",
+    },
+    {   
+         //Member of IT  Index 9
+        name: "Khyle Myrvin Macasilhig",
+        strand: "Information Technology",
+        id: "02000307446",
+        role: "FrontEnd/Researcher",
+        imageUrl: "",
+        
+    },
+    {
+         //Member of IT  Index 10
+        name: "Jude Cedrick Seguin",
+        strand: "Information Technology",
+        id: "",
+        role: "FrontEnd",
+        imageUrl: "",
+    }
+];  
