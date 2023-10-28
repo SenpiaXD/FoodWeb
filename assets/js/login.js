02a9e9fb1912870166be93f6949c18c3151194a1
Koni <145802334+SenpiaXD@users.noreply.github.com>
2023-10-28 17:37:36 +0800

    ...

--- assets/js/login.js
+++ assets/js/login.js
@@ -30,3 +30,17 @@ function loginValidation() {
         Swal.fire('Incorrect!', 'Wrong username or password', 'error');
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const passwordInput = document.getElementById("password");
+    const togglePassword = document.querySelector(".toggle-password");
+
+    togglePassword.addEventListener("click", function () {
+        const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+        passwordInput.setAttribute("type", type);
+
+        this.querySelector("i").classList.toggle("fa-eye");
+        this.querySelector("i").classList.toggle("fa-eye-slash");
+    });
+});
