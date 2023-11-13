ead46bb37ffa110c1a9c7c3086de8115a90c60af
Koni <145802334+SenpiaXD@users.noreply.github.com>
2023-11-13 21:07:21 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,3 +95,19 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+// Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+}
