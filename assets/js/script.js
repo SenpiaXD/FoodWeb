d9e2783f4aefab1bb6f4fe4bae262df2ed4d7da9
Koni <145802334+SenpiaXD@users.noreply.github.com>
2023-11-14 06:17:00 +0800

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,18 +96,24 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-// Accordion
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        } 
-    });
-}
+
+// Accordion
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            } 
+        });
+    }
+});
